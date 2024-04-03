eaa0766a89ca068a439bcafa82f5135b4a15bc2e
try-agaaain <2629757717@qq.com>
2024-04-04 00:06:25 +0800

    实现多级反馈调度

--- code/os/10-swtimer/sched.c
+++ code/os/10-swtimer/sched.c
@@ -10,7 +10,6 @@ extern void switch_to(struct context *next);
  * is always 16-byte aligned.
  */
 uint8_t __attribute__((aligned(16))) task_stack[MAX_TASKS][STACK_SIZE];
-struct context ctx_tasks[MAX_TASKS];
 
 /*
  * _top is used to mark the max available position of ctx_tasks
@@ -18,6 +17,33 @@ struct context ctx_tasks[MAX_TASKS];
  */
 static int _top = 0;
 static int _current = -1;
+
+// os.h 文件中添加
+#define MAX_PRIORITY_LEVELS 3
+#define TIME_QUANTUM {2, 4, 8} // 时间片配额，每个优先级的时间片数量
+
+struct task_struct {
+    struct context ctx; // 任务的上下文信息
+    int priority;       // 任务的当前优先级
+    int time_slice;     // 任务的剩余时间片
+    int valid;          // 任务是否有效
+};
+struct task_struct ctx_tasks[MAX_TASKS];
+
+
+struct priority_queue {
+    struct task_struct *tasks[MAX_TASKS]; // 优先级队列中的任务数组
+    int task_count;                       // 队列中的任务数量
+};
+
+// 优先级队列数组，每个元素代表一个优先级队列
+struct priority_queue priority_queues[MAX_PRIORITY_LEVELS];
+
+// 当前运行的任务
+struct task_struct *current_task = NULL;
+
+// 时间片配额数组
+const int time_quantum[MAX_PRIORITY_LEVELS] = TIME_QUANTUM;
 
 void sched_init()
 {
@@ -27,19 +53,69 @@ void sched_init()
 	w_mie(r_mie() | MIE_MSIE);
 }
 
+struct task_struct *dequeue_task(struct priority_queue *queue) {
+    if (queue->task_count == 0) {
+        return NULL;
+    }
+
+    struct task_struct *task = queue->tasks[0];
+    // 将队列中的其他任务向前移动一位
+    for (int i = 1; i < queue->task_count; i++) {
+        queue->tasks[i - 1] = queue->tasks[i];
+    }
+    queue->task_count--;
+
+    return task;
+}
+
+void enqueue_task(struct task_struct *task) {
+    int priority = task->priority;
+    if (priority_queues[priority].task_count < MAX_TASKS) {
+        priority_queues[priority].tasks[priority_queues[priority].task_count++] = task;
+    } else {
+        panic("Priority queue is full!");
+    }
+}
+
 /*
  * implment a simple cycle FIFO schedular
  */
-void schedule()
-{
-	if (_top <= 0) {
-		panic("Num of task should be greater than zero!");
-		return;
-	}
+// os.c 文件中添加或修改
+void schedule() {
+    if(current_task)
+        printf("剩余时间片：%d\n", current_task->time_slice);
 
-	_current = (_current + 1) % _top;
-	struct context *next = &(ctx_tasks[_current]);
-	switch_to(next);
+    if (current_task && (--(current_task->time_slice) > 0)) {
+        // 如果当前任务还有剩余时间片，则继续运行
+		// printf("return\n");
+        return;
+    }
+
+    if (current_task && current_task->time_slice <= 0) {
+        // 如果当前任务时间片耗尽，降低其优先级（如果不是最低优先级）
+        if (current_task->priority < MAX_PRIORITY_LEVELS - 1) {
+            current_task->priority++;
+        }
+        current_task->time_slice = time_quantum[current_task->priority];
+        enqueue_task(current_task); // 将当前任务重新放入队列
+    }
+
+    // 选择下一个要运行的任务
+    for (int i = 0; i < MAX_PRIORITY_LEVELS; i++) {
+        if (priority_queues[i].task_count > 0) {
+            current_task = dequeue_task(&priority_queues[i]);
+            // printf("addr: %x, slice: %d", current_task, current_task->time_slice);
+            break;
+        }
+    }
+	struct context *p = &current_task->ctx;
+	printf("switch: %x\n", p->pc);
+    // 切换到选定的任务
+    if (current_task) {
+        switch_to(&current_task->ctx);
+    } else {
+        panic("No more tasks to schedule!");
+    }
 }
 
 /*
@@ -50,18 +126,21 @@ void schedule()
  * 	0: success
  * 	-1: if error occured
  */
-int task_create(void (*start_routin)(void))
-{
-	if (_top < MAX_TASKS) {
-		ctx_tasks[_top].sp = (reg_t) &task_stack[_top][STACK_SIZE];
-		ctx_tasks[_top].pc = (reg_t) start_routin;
-		_top++;
-		return 0;
-	} else {
-		return -1;
-	}
+int task_create(void (*start_routine)(void)) {
+    if (_top < MAX_TASKS) {
+        struct task_struct *new_task = &ctx_tasks[_top];
+        new_task->ctx.sp = (reg_t) &task_stack[_top][STACK_SIZE];
+        new_task->ctx.pc = (reg_t) start_routine;
+        new_task->priority = 0; // 新任务开始时具有最高优先级
+        new_task->time_slice = time_quantum[0]; // 初始化时间片
+        new_task->valid = 1; // 标记为有效任务
+        enqueue_task(new_task);
+        _top++;
+        return 0;
+    } else {
+        return -1;
+    }
 }
-
 /*
  * DESCRIPTION
  * 	task_yield()  causes the calling task to relinquish the CPU and a new 
--- code/os/10-swtimer/user.c
+++ code/os/10-swtimer/user.c
@@ -8,7 +8,10 @@ struct userdata {
 };
 
 /* Jack must be global */
-struct userdata person = {0, "Jack"};
+struct userdata person0 = {0, "Jack"};
+struct userdata person1 = {0, "Mark"};
+struct userdata person2 = {0, "Jay"};
+
 
 void timer_func(void *arg)
 {
@@ -25,15 +28,15 @@ void user_task0(void)
 {
 	uart_puts("Task 0: Created!\n");
 
-	struct timer *t1 = timer_create(timer_func, &person, 3);    // 第3个tick触发
+	struct timer *t1 = timer_create(timer_func, &person0, 3);    // 第3个tick触发
 	if (NULL == t1) {
 		printf("timer_create() failed!\n");
 	}
-	struct timer *t2 = timer_create(timer_func, &person, 1);    // 第1个个tick触发
+	struct timer *t2 = timer_create(timer_func, &person1, 1);    // 第1个个tick触发
 	if (NULL == t2) {
 		printf("timer_create() failed!\n");
 	}
-	struct timer *t3 = timer_create(timer_func, &person, 6);    // 第6个tick触发
+	struct timer *t3 = timer_create(timer_func, &person2, 6);    // 第6个tick触发
 	if (NULL == t3) {
 		printf("timer_create() failed!\n");
 	}
@@ -52,10 +55,21 @@ void user_task1(void)
 	}
 }
 
+void user_task2(void)
+{
+	uart_puts("Task 2: Created!\n");
+	while (1) {
+		uart_puts("Task 2: Running... \n");
+		task_delay(DELAY);
+	}
+}
+
 /* NOTICE: DON'T LOOP INFINITELY IN main() */
 void os_main(void)
 {
+	printf("task0: %x, task1: %x\n", user_task0, user_task1);
 	task_create(user_task0);
 	task_create(user_task1);
+	task_create(user_task2);
 }
 
